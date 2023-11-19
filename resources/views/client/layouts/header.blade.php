6eadbdff426a06e31fd3ee1001f93d56e6f87ed6
Alamin <alamin017514@gmail.com>
2023-11-19 09:48:03 +0600

    overall setup done

---
+++ resources/views/client/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Antomi - Electronics eCommerce</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('client/img/favicon.ico')}}">
+
+    <!-- CSS 
+       
+    ========================= -->
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="{{asset('client/css/plugins.css')}}">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="{{asset('client/css/style.css')}}">
+
+</head>
---
+++ resources/views/client/layouts/main.blade.php
@@ -0,0 +1,12 @@
+@include('client.layouts.header')
+
+<body>
+	<div class="body">
+		@include('client.layouts.menu')
+
+		
+			@yield('content')
+       
+    
+
+    @include('client.layouts.footer')
